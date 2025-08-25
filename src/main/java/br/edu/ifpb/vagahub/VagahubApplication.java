70f3c9ce254f8ff3e10f9448d4ee80e3a240f4bb
Sofia Leal <156724109+sofialeals@users.noreply.github.com>
2025-08-25 16:28:51 -0300

    Permite, configura e implementa task scheduling

--- src/main/java/br/edu/ifpb/vagahub/VagahubApplication.java
+++ src/main/java/br/edu/ifpb/vagahub/VagahubApplication.java
@@ -2,8 +2,10 @@ package br.edu.ifpb.vagahub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class VagahubApplication {
 
 	public static void main(String[] args) {
