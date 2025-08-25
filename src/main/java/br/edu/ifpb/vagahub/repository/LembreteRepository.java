2ee3ee160353a0cd92895a2ea8555f8168ad3c06
Sofia Leal <156724109+sofialeals@users.noreply.github.com>
2025-08-25 16:25:40 -0300

    Cria LembreteRepository

---
+++ src/main/java/br/edu/ifpb/vagahub/repository/LembreteRepository.java
@@ -0,0 +1,8 @@
+package br.edu.ifpb.vagahub.repository;
+
+import br.edu.ifpb.vagahub.model.Lembrete;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LembreteRepository extends JpaRepository<Lembrete, Long> {
+
+}
